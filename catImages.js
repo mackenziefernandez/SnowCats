934ea874a288c17d9477a660670164b902269d3d
Kelsey M Niemeyer <kelsey@Kelseys-MacBook-Pro.local>
2016-02-27 11:21:44 -0700

    Added first two cat images

--- catImages.js
+++ catImages.js
@@ -1,2 +1,2 @@
-var cats = ['put imageURL here', 'put imageURL here'];
+var cats = ['http://tse4.mm.bing.net/th?id=OIP.M5904f46c2eb99f7faf0957f480d7876do0&w=230&h=170&rs=1&pcl=dddddd&pid=1.1', 'http://2.bp.blogspot.com/-B_1wK2hf7-Q/T9AkyOHyBdI/AAAAAAAABWE/UdCLWEifu7I/s1600/funny-pictures-kitten-wants-to-play-in-the-snow.jpg'];
 var snowcats = ['put imageURL here'];
